d3379cbcbeb8523e34e388e2e2f7c6fbc921e874
Jasonhaaa <103395333+Jasonhaaa@users.noreply.github.com>
2022-05-04 15:48:37 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+/* Applying the "open" class when you click the Menu button, which triggers the mobile menu to open/close; Toggle Menu/Close menu text. */
+$(document).ready(function() {
+  $('.mobile-button a').click(function(e) {
+	$(this).parent().parent().toggleClass('open');
+  $(this).html($(this).html() == 'Close Menu' ? 'Menu' : 'Close Menu');
+    e.preventDefault();
+  });
+});
+var myIndex = 0;
+carousel();
+
+
+
+function validateform(){  
+var Firstname=document.myform.Firstname.value;
+var Lastname=document.myform.Lastname.value;  
+var Suggestion=document.myform.Suggestion.value; 
+var Tel=document.myform.Tel.value;  
+  
+if (Firstname==null || Firstname==""){  
+  alert("First Name can't be blank");  
+  return false;  
+}
+
+if (Lastname==null || Lastname==""){  
+  alert("Last Name can't be blank");  
+  return false;  
+}
+
+ if (Suggestion==null || Suggestion==""){  
+  alert("Suggestion can't be blank");  
+  return false;  
+}
+else if (isNaN(Tel)){
+ alert("Please enter the number in Tel")
+  return false;
+}
+if (Tel==null || Tel==""){  
+  alert("Tel can't be blank");  
+  return false;  
+}
+}  
+
+function NavigateToSite(){
+  var ddl = document.getElementById("ddlMyList");
+  var selectedVal = ddl.options[ddl.selectedIndex].value;
+
+  window.location = selectedVal;
+}
